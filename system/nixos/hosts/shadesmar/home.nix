e1a2cc5d804d85d34e336aa329a4fda7b672a64d
Yves Ineichen <iff@yvesineichen.com>
2025-01-08 16:56:24 +0100

    shadesmare: cleanup home

--- system/nixos/hosts/shadesmar/home.nix
+++ system/nixos/hosts/shadesmar/home.nix
@@ -1,45 +1,14 @@
 { pkgs, ... }:
 
-let
-  # TODO see https://docs.astral.sh/uv/configuration/environment/
-  uvBin = pkgs.writeScriptBin "uv" ''
-    #!${pkgs.zsh}/bin/zsh
-    set -eu -o pipefail
-    export path=(${pkgs.python313}/bin ${pkgs.python310}/bin $path)
-    export UV_PYTHON_PREFERENCE=only-system
-    export UV_CACHE_DIR=/scratch/.cache/uv
-    ${pkgs.uv}/bin/uv $@
-  '';
-  uvxBin = pkgs.writeScriptBin "uvx" ''
-    #!${pkgs.zsh}/bin/zsh
-    set -eu -o pipefail
-    export path=(${pkgs.python313}/bin ${pkgs.python310}/bin $path)
-    export UV_PYTHON_PREFERENCE=only-system
-    export UV_CACHE_DIR=/scratch/.cache/uv
-    ${pkgs.uv}/bin/uvx $@
-  '';
-  wrapped-uv = pkgs.symlinkJoin {
-    name = "wrapped-uv";
-    # NOTE symlinkJoin allows clashes, the first in the list wins
-    paths = [ uvBin uvxBin pkgs.uv ];
-  };
-in
 {
   home.packages = with pkgs; [
     _1password-gui
-    awscli2
     geeqie
-    google-chrome
     lnav
-    spotify
-    sshfs
-    ssm-session-manager-plugin
     syncthing
-    # TODO below should be in direnv
-    # but linking fails due to python pollution
-    # cmake
-    # eigen
-    # wrapped-uv
+    # work
+    awscli2
+    ssm-session-manager-plugin
   ];
 
   dots = {
