d03c5fdbcfb33c0fe8127cfc0b0e7499cdf48e7f
Yves Ineichen <iff@yvesineichen.com>
2025-01-08 15:42:32 +0100

    zfs

--- system/nixos/hosts/shadesmar/hardware.nix
+++ system/nixos/hosts/shadesmar/hardware.nix
@@ -6,46 +6,68 @@
     kernelModules = [ "kvm-intel" ];
     extraModulePackages = [ ];
     initrd = {
-      availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" ];
+      availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
       kernelModules = [ "dm-snapshot" ];
     };
 
-    # loader = {
-    #   efi = {
-    #     canTouchEfiVariables = true;
-    #     efiSysMountPoint = "/boot/efi";
-    #   };
-    #   systemd-boot.enable = true;
-    # };
+    # see https://openzfs.github.io/openzfs-docs/man/v2.3/4/zfs.4.html or man zfs 4
+    # see https://wiki.nixos.org/wiki/ZFS
+    # needed for reflink to work (pool also needs to have it enabled)
+    # maybe also consider "zfs_bclone_wait_dirty"
+    # TODO this works, but is it mergeable? using environment.etc."modprobe.d/zfs.conf".text didnt have an effect
+    extraModprobeConfig = ''
+      options zfs zfs_bclone_enabled=1
+    '';
+
+    loader = {
+      timeout = 0; # TODO or null for wait, or 0 for on-key?
+      systemd-boot = {
+        enable = true;
+        configurationLimit = 100;
+        consoleMode = "0"; # all options often take long to appear
+      };
+      efi.canTouchEfiVariables = true;
+    };
+    # TODO does this add kernel modules? can i set options?
+    supportedFilesystems = [ "zfs" ];
 
     # for now use GRUB to dual boot with ubuntu
-    loader = {
-      efi = {
-        canTouchEfiVariables = true;
-        efiSysMountPoint = "/boot/efi";
-      };
-      grub = {
-        efiSupport = true;
-        device = "nodev";
-        useOSProber = true;
-      };
-    };
-
+    #   loader = {
+    #     efi = {
+    #       canTouchEfiVariables = true;
+    #       efiSysMountPoint = "/boot/efi";
+    #     };
+    #     grub = {
+    #       efiSupport = true;
+    #       device = "nodev";
+    #       useOSProber = true;
+    #     };
+    #   };
+    #
   };
 
-  swapDevices = [ ];
+  networking.hostId = "e163c59c";
 
-  fileSystems."/" =
-    {
-      device = "/dev/disk/by-uuid/38a43ffb-f216-46b6-a3ef-7619d65c215a";
-      fsType = "btrfs";
-      options = [ "noatime" "ssd" "discard=async" "space_cache=v2" "commit=120" ];
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  fileSystems = {
+    "/" = {
+      device = "system/state";
+      fsType = "zfs";
     };
-  fileSystems."/boot/efi" =
-    {
-      device = "/dev/disk/by-uuid/54EC-62DC";
+    "/boot" = {
+      device = "/dev/disk/by-uuid/2CDB-0C1A";
       fsType = "vfat";
+      options = [ "fmask=0077" "dmask=0077" ];
+      depends = [ "/" ];
     };
+    "/nix" = {
+      device = "system/nix";
+      fsType = "zfs";
+      depends = [ "/" ];
+    };
+  };
+
   fileSystems."/scratch" =
     {
       device = "/dev/disk/by-uuid/923aa534-f79d-43fa-8532-fc6a5f0cfd6a";
@@ -57,19 +79,28 @@
       device = "/dev/disk/by-uuid/dd44e584-a85b-481e-9772-339b1c6ecb7b";
       fsType = "ext4";
     };
-  fileSystems."/uhome" =
-    {
-      device = "/dev/disk/by-uuid/7c79a22d-2996-4638-9106-08e957c1d722";
-      fsType = "ext4";
-      options = [ "ro" ];
-    };
 
-  services.btrfs.autoScrub = {
-    enable = true;
-    interval = "weekly";
-    fileSystems = [ "/" "/scratch" ];
-  };
-
-  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  # fileSystems."/" =
+  #   {
+  #     device = "/dev/disk/by-uuid/38a43ffb-f216-46b6-a3ef-7619d65c215a";
+  #     fsType = "btrfs";
+  #     options = [ "noatime" "ssd" "discard=async" "space_cache=v2" "commit=120" ];
+  #   };
+  # fileSystems."/boot/efi" =
+  #   {
+  #     device = "/dev/disk/by-uuid/54EC-62DC";
+  #     fsType = "vfat";
+  #   };
+  # fileSystems."/uhome" =
+  #   {
+  #     device = "/dev/disk/by-uuid/7c79a22d-2996-4638-9106-08e957c1d722";
+  #     fsType = "ext4";
+  #     options = [ "ro" ];
+  #   };
+  #
+  # services.btrfs.autoScrub = {
+  #   enable = true;
+  #   interval = "weekly";
+  #   fileSystems = [ "/" "/scratch" ];
+  # };
 }
