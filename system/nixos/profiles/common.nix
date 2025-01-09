45338199307c4fd5a5f5057eb8599795cab15289
Yves Ineichen <iff@yvesineichen.com>
2025-01-09 14:06:21 +0100

    shadesmar: sudo w/o pw for some commands

--- system/nixos/profiles/common.nix
+++ system/nixos/profiles/common.nix
@@ -44,6 +44,23 @@
       vim
     ];
 
+    security.sudo = {
+      enable = true;
+      extraRules = [{
+        commands = [
+          {
+            command = "${pkgs.systemd}/bin/reboot";
+            options = [ "NOPASSWD" ];
+          }
+          {
+            command = "${pkgs.systemd}/bin/poweroff";
+            options = [ "NOPASSWD" ];
+          }
+        ];
+        groups = [ "wheel" ];
+      }];
+    };
+
     nix = {
       settings = {
         auto-optimise-store = true;
