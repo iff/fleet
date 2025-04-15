713eac27f78cd2a2ba4a86c3e09b99e976cddf9c
Yves Ineichen <iff@yvesineichen.com>
2025-04-15 21:13:01 +0200

    nvim: telescope todos

--- home/modules/editor/nvim/lua/yi/telescope.lua
+++ home/modules/editor/nvim/lua/yi/telescope.lua
@@ -42,6 +42,104 @@ local function at_top()
     vim.cmd([[normal! zt]])
 end
 
+-- TODO
+function M.git_diff_files(opts)
+    local pickers = require("telescope.pickers")
+    local finders = require("telescope.finders")
+    local conf = require("telescope.config").values
+    local list = vim.fn.systemlist("git diff --name-only master 2>/dev/null | git diff --name-only main")
+
+    pickers
+        .new(opts, {
+            prompt_title = "git diff to main/master",
+            finder = finders.new_table { results = list },
+            sorter = conf.generic_sorter(opts),
+        })
+        :find()
+end
+
+-- TODO
+local function laforge()
+    require("telescope.pickers.layout_strategies").laforge = function(self, max_columns, max_lines, layout_config)
+        -- local resolve = require("telescope.config.resolve")
+        -- local p_window = require("telescope.pickers.window")
+        -- local initial_options = p_window.get_initial_window_options(self)
+        -- local results = initial_options.results
+        -- local prompt = initial_options.prompt
+        -- local preview = initial_options.preview
+        local half = vim.fn.round(max_lines / 2)
+        local pad = 3
+        return {
+            preview = {
+                border = true,
+                borderchars = { "─", "│", "═", "│", "┌", "┐", "╛", "╘" },
+                col = 2,
+                enter = false,
+                height = half - pad - 3,
+                line = 2,
+                width = max_columns - 2,
+            },
+            prompt = {
+                border = true,
+                borderchars = { "═", "│", "─", "│", "╒", "╕", "│", "│" },
+                col = 2,
+                enter = true,
+                height = 1,
+                line = half + pad + 2,
+                title = self.prompt_title,
+                width = max_columns - 2,
+            },
+            results = {
+                border = { 0, 1, 1, 1 },
+                borderchars = { "═", "│", "─", "│", "╒", "╕", "┘", "└" },
+                col = 2,
+                enter = false,
+                height = max_lines - half - pad - 4,
+                line = half + pad + 4,
+                width = max_columns - 2,
+            },
+        }
+    end
+
+    -- local defaults = require('telescope.themes').get_ivy({ layout_config = { height = 0.4 } })
+    local defaults = {
+        -- TODO how to make the layout strat and the rest go together? many things are not independent, like sorting_strategy
+        layout_strategy = "laforge",
+        sorting_strategy = "ascending",
+        prompt_prefix = "󰄾 ",
+        entry_prefix = "   ",
+        selection_caret = " 󰧚 ",
+    }
+    defaults.scroll_strategy = "limit"
+    defaults.mappings = {
+        i = {
+            ["<c-j>"] = actions.move_selection_next,
+            ["<c-k>"] = actions.move_selection_previous,
+            ["<enter>"] = actions.select_default,
+            ["<c-v>"] = actions.select_vertical,
+            ["<c-s>"] = actions.select_horizontal,
+            ["<c-t>"] = actions.select_tab,
+            ["<tab>"] = actions.toggle_selection + actions.move_selection_next,
+            ["<s-tab>"] = actions.toggle_all,
+            ["<c-f>"] = function(prompt_bufnr)
+                telescope.extensions.hop._hop(prompt_bufnr, { callback = actions.select_default })
+            end,
+        },
+        n = {
+            ["q"] = actions.close,
+            ["y"] = "move_selection_next",
+            ["l"] = "move_selection_previous",
+            ["<enter>"] = actions.select_default,
+            ["n"] = actions.select_vertical,
+            ["u"] = actions.select_horizontal,
+            ["t"] = actions.select_tab,
+            ["<tab>"] = actions.toggle_selection + actions.move_selection_next,
+            ["<s-tab>"] = actions.toggle_all,
+        },
+    }
+    defaults.path_display = { "truncate" }
+end
+
 local function jump_find_files(entry)
     -- { "config/lua/dk/mappings.lua",
     --   index = 4,
