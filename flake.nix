8e49b5e14e3b81c52f9e1cfa90423e260d20e12b
Yves Ineichen <iff@yvesineichen.com>
2025-05-21 14:37:51 +0200

    zen: update

--- flake.nix
+++ flake.nix
@@ -41,7 +41,7 @@
       flake = false;
       # see https://github.com/zen-browser/desktop/releases
       # TODO eventually https://github.com/NixOS/nixpkgs/issues/327982
-      url = "tarball+https://github.com/zen-browser/desktop/releases/download/1.11.1b/zen.linux-x86_64.tar.xz";
+      url = "tarball+https://github.com/zen-browser/desktop/releases/download/1.12.5b/zen.linux-x86_64.tar.xz";
     };
 
 
