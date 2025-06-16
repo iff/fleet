baf25187fcf5636dcdd7f669d5a8347e19013ce4
Yves Ineichen <iff@yvesineichen.com>
2025-06-16 21:48:54 +0200

    swaylock: add config

--- home/profiles/hyprland.nix
+++ home/profiles/hyprland.nix
@@ -14,26 +14,6 @@ in
       pkgs.swaylock-effects # see https://github.com/jirutka/swaylock-effects
     ];
 
-    programs.swaylock = {
-      # enable = true;
-      settings = {
-        screenshots = true;
-        clock = true;
-        indicator = true;
-        indicator-radius = 100;
-        indicator-thickness = 7;
-        effect-blur = "7x5";
-        effect-vignette = "0.5:0.5";
-        # color = "00000000";
-        ring-color = "3b4252";
-        key-hl-color = "880033";
-        line-color = "00000000";
-        inside-color = "00000088";
-        separator-color = "00000000";
-        # grace = 2;
-      };
-    };
-
     services.wlsunset = {
       enable = true;
       latitude = "47.4";
@@ -61,6 +41,7 @@ in
     };
 
     xdg.configFile = {
+      "swaylock/config".source = hypr/swaylock.config;
       "waybar/config.jsonc".source = hypr/waybar.jsonc;
       "waybar/style.css".source = hypr/waybar.css;
       "hypr/hyprland.conf".source = hypr/hyprland.conf;
