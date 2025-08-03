3c9483de6aa995a416f1048b665a246f2a1ab0ad
Yves Ineichen <iff@yvesineichen.com>
2025-08-03 08:04:31 +0200

    kharbrant: switch script

--- system/nixos/hosts/kharbranth/home.nix
+++ system/nixos/hosts/kharbranth/home.nix
@@ -1,16 +1,28 @@
 { pkgs, ... }:
 
+let
+  switch = pkgs.writeScriptBin "switch"
+    ''
+      #!/usr/bin/env zsh
+      set -eu -o pipefail
+
+      sudo nixos-rebuild switch --flake .
+    '';
+in
 {
   home.packages = with pkgs; [
     # ‘libsoup-2.74.3’ is marked as insecure, refusing to evaluate
     # geeqie
     google-chrome
     neovide
+    nvd
     protonmail-desktop
     roam-research
     spotify
     transmission_4-gtk
     vlc
+    #
+    switch
   ];
 
   dots = {
