836df576b973354bc9f5a23ae4096a03426b1787
Yves Ineichen <iff@yvesineichen.com>
2026-02-03 08:08:54 +0100

    zsh: alias jj

--- home/modules/shell/zsh/zshrcd/aliases.zsh
+++ home/modules/shell/zsh/zshrcd/aliases.zsh
@@ -15,7 +15,7 @@ alias ls="eza --header --git --time-style=relative --icons --no-permissions --no
 alias lr="ls --sort=newest"
 alias la="eza --long --header --all --icons --git"
 alias ll="eza --header --git --time-style=long-iso --icons --group --long --mounts --sort=name"
-alias md="mkdir -p"
 alias man="man --no-justification"
 alias k="kubectl"
+alias nn="jj"
 (( $+commands[docker] )) && alias dk="docker kill $(docker ps -q)"
