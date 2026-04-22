ca934b1f37a3425e6913de303fc6eb65a2bbc0ec
Yves Ineichen <iff@yvesineichen.com>
2026-04-22 09:50:19 +0200

    tmux: leaner theme and less info in git prompt

--- home/modules/programs/tmux.nix
+++ home/modules/programs/tmux.nix
@@ -41,9 +41,9 @@ in
             set -g status-right-length 50
             set -g status-justify "left"
 
-            set -g mode-style "fg=#232831,bg=#abb1bb"
-            set -g message-style "fg=#232831,bg=#abb1bb"
-            set -g message-command-style "fg=#232831,bg=#abb1bb"
+            set -g mode-style "fg=#abb1bb,bg=#232831"
+            set -g message-style "fg=#abb1bb,bg=#232831"
+            set -g message-command-style "fg=#abb1bb,bg=#232831"
 
             set -g pane-border-style "fg=#abb1bb"
             set -g pane-active-border-style "fg=#81a1c1"
@@ -53,16 +53,16 @@ in
             set -g status-style "fg=#abb1bb,bg=#232831"
 
             set -g status-left-style NONE
-            set -g status-left "#[fg=#232831,bg=#81a1c1,bold] #{session_name} #(tmux-git-prompt) #[fg=#81a1c1,bg=#232831,nobold,nounderscore,noitalics]"
+            set -g status-left "#[fg=#81a1c1,bg=#232831,bold] #{session_name} #(tmux-git-prompt) "
 
             set -g status-right-style NONE
-            set -g status-right "#[fg=#232831,bg=#232831,nobold,nounderscore,noitalics][fg=#81a1c1,bg=#232831] #{prefix_highlight} #[fg=#abb1bb,bg=#232831,nobold,nounderscore,noitalics]#[fg=#232831,bg=#abb1bb] %H:%M #[fg=#81a1c1,bg=#abb1bb,nobold,nounderscore,noitalics]#[fg=#232831,bg=#81a1c1,bold] #h "
+            set -g status-right "#[fg=#81a1c1,bg=#232831] #{prefix_highlight} #[fg=#abb1bb,bg=#232831] %H:%M #[fg=#81a1c1,bg=#232831,bold] #h "
 
             setw -g window-status-activity-style "underscore,fg=#7e8188,bg=#232831"
             setw -g window-status-separator ""
             setw -g window-status-style "NONE,fg=#7e8188,bg=#232831"
-            setw -g window-status-format "#[fg=#232831,bg=#232831,nobold,nounderscore,noitalics]#[default] #I  #W #[fg=#232831,bg=#232831,nobold,nounderscore,noitalics]"
-            setw -g window-status-current-format "#[fg=#232831,bg=#abb1bb,nobold,nounderscore,noitalics]#[fg=#232831,bg=#abb1bb,bold] #I  #W #[fg=#abb1bb,bg=#232831,nobold,nounderscore,noitalics]"
+            setw -g window-status-format "#[fg=#7e8188,bg=#232831] #I  #W "
+            setw -g window-status-current-format "#[fg=#c9826b,bg=#232831,bold] #I  #W "
 
             # only show pane bare if more than one
             set-hook -g -w pane-focus-in { set-option -Fw pane-border-status '#{?#{e|>:#{window_panes},1},top,off}' }
