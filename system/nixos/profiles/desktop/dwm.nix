7f24f1aa40b7f4b2a59dd8f41075746ff23d3a65
Yves Ineichen <iff@yvesineichen.com>
2026-04-22 09:50:19 +0200

    dwm: fix xorg package deprecation

--- system/nixos/profiles/desktop/dwm.nix
+++ system/nixos/profiles/desktop/dwm.nix
@@ -64,7 +64,7 @@ in
   config = mkIf (cfg.enable && (cfg.wm == "dwm" || cfg.wm == "all")) {
     environment.systemPackages = with pkgs; [
       inotify-tools
-      xorg.xinit
+      xinit
     ];
 
     services.xserver = {
